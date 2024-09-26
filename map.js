b3ae8d5a8344c7e31a0ec4b1c2fcef0065c91b13
Yasidu Pathiraja <yasindupathiraja10@gmail.com>
2024-09-26 15:22:10 +0530

    responsive 1

--- map.js
+++ map.js
@@ -2,7 +2,7 @@ function geoFindMe() {
     function success(position) {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-  
+      
       window.location.href=`https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
 
     }
@@ -11,3 +11,5 @@ function geoFindMe() {
   
   document.getElementById("btnOnLocationOnAction").addEventListener("click", geoFindMe);
 
+
+  // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDIuS9U2f3q-bCcapt8s_KPrjGobiiTzlg
